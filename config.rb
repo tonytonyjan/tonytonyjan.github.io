2aa565c451e133c9fdbcfd71cc62022f2297e0ab
Jian Weihang <tonytonyjan@gmail.com>
2014-12-25 03:50:23 +0800

    add timezone

--- config.rb
+++ config.rb
@@ -1,14 +1,22 @@
 set :site_url, 'http://tonytonyjan.net'
 
-Time.zone = 'Asia/Taipei'
 set :markdown_engine, :kramdown
 set :markdown, input: :GFM
+set :time_zone, 'Taipei'
+
+
 activate :i18n, mount_at_root: :'zh-TW'
 activate :blog do |blog|
   blog.layout   = 'blog'
   blog.sources  = 'posts/{year}-{month}-{day}-{title}.html'
   blog.paginate = true
-  blog.per_page = 10
   blog.summary_separator = /<!-- more -->/
+  blog.summary_length    = 150
 end
 activate :directory_indexes
+
+helpers do
+ def condense str
+  str.gsub(/[\s\n]+/, ' ') if str
+ end
+end
