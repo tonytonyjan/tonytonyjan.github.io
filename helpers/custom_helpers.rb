06b9e7c3f1552910053608a6776e2dc0de042064
Jian Weihang <tonytonyjan@gmail.com>
2015-01-04 00:03:44 +0800

    update

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -10,4 +10,13 @@ module CustomHelpers
   def article_description
     current_article.data.description || condense(strip_tags(current_article.summary))
   end
+
+  def article_thumb_path article
+    return article.data.thumb if article.data.thumb
+
+    default_thumb_path = "thumbs/#{article.data.image}"
+    return default_thumb_path if File.exist?(File.join(source_dir, "images/#{default_thumb_path}"))
+
+    return "thumbs/兜-1.png"
+  end
 end
