0908c403e29a143de075bcbcaf87f8e067c4b0ac
Jian Weihang <tonytonyjan@gmail.com>
2016-09-17 20:50:49 +0800

    update

--- config.rb
+++ config.rb
@@ -1,13 +1,20 @@
+# frozen_string_literal: true
 set :site_name, '不歸錄'
 set :site_url, 'https://tonytonyjan.net'
-set :blog_description, '大兜的個人部落格，本名簡煒航，網路代號 tonytonyjan。內容包括程式設計、音樂創作與生活雜記。'.freeze
-
+set :blog_description, '大兜的個人部落格，本名簡煒航，網路代號 tonytonyjan。內容包括程式設計、音樂創作與生活雜記。'
 set :markdown, input: :GFM
 set :time_zone, 'Taipei'
 
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page 'google7bd685dfb8300eb0.html', directory_index: false
+page '404.html', directory_index: false
 
-activate :livereload
-activate :i18n, langs: %i[zh-TW en]
+configure :development do
+  activate :livereload
+end
+activate :i18n, langs: %i(zh-TW en)
 activate :directory_indexes
 
 activate :blog do |blog|
@@ -25,18 +32,14 @@ activate :blog do |blog|
   }
 end
 
-# activate :deploy do |deploy|
-#   deploy.method = :rsync
-#   deploy.clean = true
-#   deploy.user            = 'deploy'
-#   deploy.host            = '128.199.147.210'
-#   deploy.port            = 22
-#   deploy.path            = '/var/www/tonytonyjan.net'
-# end
-
-page 'feed.xml', layout: false
-page 'google7bd685dfb8300eb0.html', directory_index: false
-page '404.html', directory_index: false
+activate :deploy do |deploy|
+  deploy.deploy_method = :rsync
+  deploy.clean = true
+  deploy.user = 'deploy'
+  deploy.host = 'tonytonyjan.net'
+  deploy.port = 22
+  deploy.path = '/var/www/tonytonyjan.net'
+end
 
 ignore 'slides/*'
 
--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
 module CustomHelpers
   def page_image
     config[:site_url] + image_path(current_page.data.image || 'site/avatar_big.png')
   end
 
-  def condense str
+  def condense(str)
     str.gsub(/[\s\n]+/, ' ') if str
   end
 
@@ -15,7 +16,7 @@ module CustomHelpers
     current_article.data.description || condense(strip_tags(current_article.summary))
   end
 
-  def article_thumb_path article
+  def article_thumb_path(article)
     if article.data.thumb
       article.data.thumb
     elsif article.data.image && File.exist?(File.join(source_dir, "images/thumbs/#{article.data.image}"))
