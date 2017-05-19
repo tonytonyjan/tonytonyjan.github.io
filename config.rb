49420603f6190a5cb2e52a4d4e7d253bec7f4b0b
Jian Weihang <tonytonyjan@gmail.com>
2017-05-19 14:57:41 +0800

    change user

--- config.rb
+++ config.rb
@@ -36,7 +36,7 @@ activate :deploy do |deploy|
   deploy.deploy_method = :rsync
   deploy.clean = true
   # deploy.user = 'deploy' # https://github.com/middleman-contrib/middleman-deploy/issues/79
-  deploy.host = 'deploy@128.199.147.210'
+  deploy.host = 'freebsd@128.199.147.210'
   deploy.path = '/var/www/tonytonyjan.net'
 end
 
